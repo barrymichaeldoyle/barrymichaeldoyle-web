63672c8fd760ac718710d47a0a494dea1d84cd11
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2025-06-01 20:09:35 +0200

    added shadcn

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -6,23 +6,11 @@ import appCss from '@/styles/app.css?url';
 export const Route = createRootRoute({
   head: () => ({
     meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
+      { charSet: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { title: 'Barry Michael Doyle - Staff Frontend Engineer' },
     ],
-    links: [
-      {
-        rel: 'stylesheet',
-        href: appCss,
-      },
-    ],
+    links: [{ rel: 'stylesheet', href: appCss }],
   }),
   component: RootComponent,
 });
