abd828731d32fff86cadbbe1b0d4754461e2d80a
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2025-07-29 20:36:46 +0200

    updated about section

--- src/screens/Home/sections/About/About.tsx
+++ src/screens/Home/sections/About/About.tsx
@@ -4,22 +4,60 @@ import { ProfileCard } from '~/components/reactbits/Components/ProfileCard/Profi
 import { sections } from '~/constants';
 
 export function AboutSection() {
+  const programmingYears = new Date().getFullYear() - 2013;
+
   return (
     <section
       id={sections.about}
       className="mx-auto flex w-full max-w-250 flex-col gap-12 py-36 md:flex-row md:gap-8"
     >
-      <div className="flex flex-2 flex-col gap-2 px-2">
+      <div className="flex flex-2 flex-col gap-4 px-2">
         <Link to={`#${sections.about}` as string} data-slot="button">
           <h2 className="pb-2 text-center md:text-left">About</h2>
         </Link>
         <p>
-          Hey I&apos;m Barry! As you can pretty much see. This site is still a
-          work in progress.
+          Hey, I&apos;m Barry! I&apos;ve been programming professionally for
+          over {programmingYears} years. Currently, I&apos;m a Senior Engineer
+          at{' '}
+          <a
+            href="https://securitease.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SecuritEase
+          </a>
+          .
         </p>
         <p>
-          But hey, if I&apos;m good at what I do... It&apos;ll look a lot better
-          soon!
+          I love exploring various technologies, but most of my professional
+          experience has been frontend-focused. ReactJS and React Native are my
+          go-to frameworks for web and mobile applications, with TypeScript as
+          the foundation for everything I build. That said, I&apos;m always
+          happy playing with new technologies when it means building cool things
+          or extending features on existing projects.
+        </p>
+        <p>
+          When I&apos;m not coding for work, you can find me streaming my side
+          projects on{' '}
+          <a
+            href="https://www.twitch.tv/barrymichaeldoyle"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitch
+          </a>
+          .
+        </p>
+        <p>
+          My latest side project is{' '}
+          <a
+            href="https://www.github.com/patch-pulse"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Patch Pulse
+          </a>
+          , a set of developer tools for keeping npm packages up to date.
         </p>
       </div>
       <div className="flex justify-center">
