48e33f93637edc0b6f108b0352eae284de303032
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2025-12-09 11:29:13 +0200

    fixed: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlify from '@netlify/vite-plugin-tanstack-start';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
@@ -21,6 +22,7 @@ export default defineConfig({
     tanstackStart({
       sitemap: { host: 'https://barrymichaeldoyle.com' },
     }),
+    netlify(),
     viteReact(),
     tailwindcss(),
   ],
